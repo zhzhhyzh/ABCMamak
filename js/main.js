12b9f5a382a1c7e010c4920f02b5db3765db4caf
zhzhhyzh <henryyzh0309@gmail.com>
2023-08-30 22:29:23 +0800

    Source code

---
+++ js/main.js
@@ -0,0 +1,8 @@
+// Get the button:
+let mybutton = document.getElementById("upBtn");
+
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+    window.scrollTo({top:0, left:0,behavior:'smooth'});
+}
